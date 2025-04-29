7191e9bf8ae39782cbc991673d7e7c1771d8b599
jip <83951137+jipgg@users.noreply.github.com>
2025-04-29 20:14:54 +0200

    a

--- src/type_impl/writer.cpp
+++ src/type_impl/writer.cpp
@@ -20,34 +20,34 @@ auto test_namecall_writer(lua_State* L, writer& self, int atom) -> std::optional
     };
     using named = named_atom;
     switch (static_cast<named>(atom)) {
-        case named::writestring:
+        case named::write_string:
             *self << luaL_checkstring(L, 2);
             return push_self();
-        case named::writeu8:
+        case named::write_u8:
             write_number_raw<uint8_t>(self, L);
             return push_self();
-        case named::writei8:
+        case named::write_i8:
             write_number_raw<int8_t>(self, L);
             return push_self();
-        case named::writeu16:
+        case named::write_u16:
             write_number_raw<uint16_t>(self, L);
             return push_self();
-        case named::writei16:
+        case named::write_i16:
             write_number_raw<int16_t>(self, L);
             return push_self();
-        case named::writeu32:
+        case named::write_u32:
             write_number_raw<uint32_t>(self, L);
             return push_self();
-        case named::writei32:
+        case named::write_i32:
             write_number_raw<int32_t>(self, L);
             return push_self();
-        case named::writef32:
+        case named::write_f32:
             write_number_raw<float>(self, L);
             return push_self();
-        case named::writef64:
+        case named::write_f64:
             write_number_raw<double>(self, L);
             return push_self();
-        case named::write: {
+        case named::write_buffer: {
             auto buf = lua::to_buffer(L, 2);
             self->write(&buf.front(), buf.size());
             return push_self();
@@ -56,19 +56,16 @@ auto test_namecall_writer(lua_State* L, writer& self, int atom) -> std::optional
             *self << lua::tostring_tuple(L, {.start_index = 2, .separator = "\t"});
             *self << luaL_checkstring(L, 2);
             return push_self();
-        case named::writeln: {
-            auto buf = lua::to_buffer(L, 2);
-            self->write(&buf.front(), buf.size());
-            self->put('\n');
-            return push_self();
-        }
         case named::println:
             *self << lua::tostring_tuple(L, {
                 .start_index = 2,
-                .separator = "\t"}) << '\n';
+                .separator = ", "}) << '\n';
             return push_self();
         case named::ln:
             self->put('\n');
+            return push_self();
+        case named::tab:
+            self->put('\t');
             return push_self();
         case named::endl:
             *self << std::endl;
@@ -78,6 +75,23 @@ auto test_namecall_writer(lua_State* L, writer& self, int atom) -> std::optional
             return push_self();
         case named::eof:
             return lua::push(L, self->eof());
+        case named::good:
+            return lua::push(L, self->good());
+        case named::bad:
+            return lua::push(L, self->bad());
+        case named::fail:
+            return lua::push(L, self->fail());
+        case named::clear:
+            self->clear();
+            return push_self();
+        case named::seekp:
+            self->seekp(luaL_checkinteger(L, 2));
+            return push_self();
+        case named::tellp:
+            return lua::push(L, static_cast<int>(self->tellp()));
+        case named::esc:
+            self->put('\033');
+            return push_self();
         default: return std::nullopt;
     }
 }
